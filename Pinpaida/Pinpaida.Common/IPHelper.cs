fbf884443cfbb293a99507d0dc56b68dab617d94
孙巍 <sw3964@ly.com>
2018-09-08 21:59:28 +0800

    新增帮助类

---
+++ Pinpaida/Pinpaida.Common/IPHelper.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Pinpaida.Common
+{
+    public static class IPHelper
+    {
+        /// <summary>
+        /// 获取客户端IP地址
+        /// </summary>
+        /// <returns></returns>
+        public static string GetHostAddress()
+        {
+            string userHostAddress = HttpContext.Current.Request.UserHostAddress;
+
+            if (string.IsNullOrEmpty(userHostAddress))
+            {
+                userHostAddress = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+
+            //最后判断获取是否成功，并检查IP地址的格式（检查其格式非常重要）
+            if (!string.IsNullOrEmpty(userHostAddress) && IsIP(userHostAddress))
+            {
+                return userHostAddress;
+            }
+            return "127.0.0.1";
+        }
+
+        public static bool IsIP(string ip)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+        }
+
+
+        /// <summary>
+        /// 根据IP淘宝api获取城市
+        /// </summary>
+        /// <param name="strIP"></param>
+        /// <returns></returns>
+        public static string GetIPCitys(string strIP)
+        {
+            try
+            {
+                string Url = "http://ip.taobao.com/service/getIpInfo.php?ip=" + strIP + "";
+
+                System.Net.WebRequest wReq = System.Net.WebRequest.Create(Url);
+                wReq.Timeout = 2000;
+                System.Net.WebResponse wResp = wReq.GetResponse();
+                System.IO.Stream respStream = wResp.GetResponseStream();
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(respStream))
+                {
+                    string jsonText = reader.ReadToEnd();
+                    JObject ja = (JObject)JsonConvert.DeserializeObject(jsonText);
+                    if (ja["code"].ToString() == "0")
+                    {
+                        string c = ja["data"]["city"].ToString();
+                        int ci = c.IndexOf('市');
+                        if (ci != -1)
+                        {
+                            c = c.Remove(ci, 1);
+                        }
+                        return c;
+                    }
+                    else
+                    {
+                        return "未知";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return ("未知");
+            }
+        }
+    }
+}
